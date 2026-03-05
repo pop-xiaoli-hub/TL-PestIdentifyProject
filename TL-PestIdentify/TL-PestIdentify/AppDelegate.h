00d9c38563a2702df06e64f71f29f197414589e5
popxiaoli <2397846118@qq.com>
2026-03-05 21:47:53 +0800

    commit

---
+++ TL-PestIdentify/TL-PestIdentify/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TL-PestIdentify
+//
+//  Created by xiaoli pop on 2026/3/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TL-PestIdentify/TL-PestIdentify/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TL-PestIdentify
+//
+//  Created by xiaoli pop on 2026/3/5.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+
+@end
