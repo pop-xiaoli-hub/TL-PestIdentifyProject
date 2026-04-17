e1d14ad714993ad7a0867c48cc2208782b9a289a
Northern Lights <twu49097@gmail.com>
2026-04-17 13:37:38 +0800

    设置页：系统通知权限开关与APNs注册

--- TL-PestIdentify/TL_My/Setting/TLWSettingViewController.m
+++ TL-PestIdentify/TL_My/Setting/TLWSettingViewController.m
@@ -7,10 +7,13 @@
 #import "TLWSettingView.h"
 #import "TLWSDKManager.h"
 #import "TLWPasswordLoginController.h"
+#import "TLWToast.h"
+#import <UserNotifications/UserNotifications.h>
 #import <Masonry/Masonry.h>
 
 @interface TLWSettingViewController ()
 @property (nonatomic, strong) TLWSettingView *myView;
+@property (nonatomic, assign) BOOL isUpdatingNotificationSwitch;
 @end
 
 @implementation TLWSettingViewController
@@ -31,11 +34,19 @@
     }];
     [self.view bringSubviewToFront:self.navBar];
     [self setupActions];
+    [self tl_syncNotificationSwitchState];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(tl_syncNotificationSwitchState)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
 }
 
 #pragma mark - Actions
 
 - (void)setupActions {
+    [_myView.notificationSwitch addTarget:self
+                                   action:@selector(onNotificationSwitchChanged:)
+                         forControlEvents:UIControlEventValueChanged];
     [_myView.aboutRowButton      addTarget:self action:@selector(onAbout)       forControlEvents:UIControlEventTouchUpInside];
     [_myView.feedbackRowButton   addTarget:self action:@selector(onFeedback)    forControlEvents:UIControlEventTouchUpInside];
     [_myView.permissionRowButton addTarget:self action:@selector(onPermission)  forControlEvents:UIControlEventTouchUpInside];
@@ -44,11 +55,26 @@
     [_myView.logoutButton        addTarget:self action:@selector(onLogout)      forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)onAbout       { /* TODO: 关于我们 */ }
-- (void)onFeedback    { /* TODO: 我要反馈 */ }
-- (void)onPermission  { /* TODO: 系统权限 */ }
-- (void)onAgreement   { /* TODO: 用户协议 */ }
-- (void)onPrivacy     { /* TODO: 隐私政策 */ }
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)onNotificationSwitchChanged:(UISwitch *)sender {
+    if (self.isUpdatingNotificationSwitch) {
+        return;
+    }
+    if (sender.isOn) {
+        [self tl_requestNotificationAuthorizationIfNeeded];
+    } else {
+        [self tl_handleNotificationDisableAttempt];
+    }
+}
+
+- (void)onAbout       { [TLWToast show:@"开发中..."]; }
+- (void)onFeedback    { [TLWToast show:@"开发中..."]; }
+- (void)onPermission  { [TLWToast show:@"开发中..."]; }
+- (void)onAgreement   { [TLWToast show:@"开发中..."]; }
+- (void)onPrivacy     { [TLWToast show:@"开发中..."]; }
 
 - (void)onLogout {
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"退出登录"
@@ -67,6 +93,102 @@
                            options:UIViewAnimationOptionTransitionCrossDissolve
                         animations:nil
                         completion:nil];
+    }]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - Notification Permission
+
+- (void)tl_syncNotificationSwitchState {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+        BOOL enabled = [self tl_isNotificationAuthorizedForSettings:settings];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.isUpdatingNotificationSwitch = YES;
+            [self.myView.notificationSwitch setOn:enabled animated:YES];
+            self.isUpdatingNotificationSwitch = NO;
+        });
+    }];
+}
+
+- (void)tl_requestNotificationAuthorizationIfNeeded {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    __weak typeof(self) weakSelf = self;
+    [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        if (!strongSelf) return;
+
+        if ([strongSelf tl_isNotificationAuthorizedForSettings:settings]) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [strongSelf tl_registerForRemoteNotifications];
+                [TLWToast show:@"系统通知已开启"];
+                [strongSelf tl_syncNotificationSwitchState];
+            });
+            return;
+        }
+
+        if (settings.authorizationStatus == UNAuthorizationStatusDenied) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [strongSelf tl_presentNotificationSettingsAlertWithMessage:@"系统通知权限已关闭，请前往系统设置开启。"];
+                [strongSelf tl_syncNotificationSwitchState];
+            });
+            return;
+        }
+
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge)
+                              completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (granted) {
+                    [strongSelf tl_registerForRemoteNotifications];
+                    [TLWToast show:@"系统通知已开启"];
+                } else {
+                    NSString *message = error.localizedDescription.length > 0 ? error.localizedDescription : @"请在系统设置中开启通知权限";
+                    [TLWToast show:message];
+                }
+                [strongSelf tl_syncNotificationSwitchState];
+            });
+        }];
+    }];
+}
+
+- (void)tl_handleNotificationDisableAttempt {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    __weak typeof(self) weakSelf = self;
+    [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        if (!strongSelf) return;
+
+        BOOL enabled = [strongSelf tl_isNotificationAuthorizedForSettings:settings];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [strongSelf tl_syncNotificationSwitchState];
+            if (enabled) {
+                [strongSelf tl_presentNotificationSettingsAlertWithMessage:@"iOS 不支持在应用内直接关闭系统通知，请前往系统设置关闭。"];
+            }
+        });
+    }];
+}
+
+- (BOOL)tl_isNotificationAuthorizedForSettings:(UNNotificationSettings *)settings {
+    UNAuthorizationStatus status = settings.authorizationStatus;
+    return status == UNAuthorizationStatusAuthorized
+        || status == UNAuthorizationStatusProvisional
+        || status == UNAuthorizationStatusEphemeral;
+}
+
+- (void)tl_registerForRemoteNotifications {
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+
+- (void)tl_presentNotificationSettingsAlertWithMessage:(NSString *)message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"系统消息通知"
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"去设置" style:UIAlertActionStyleDefault handler:^(__unused UIAlertAction *action) {
+        NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+        if (url && [[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+        }
     }]];
     [self presentViewController:alert animated:YES completion:nil];
 }
